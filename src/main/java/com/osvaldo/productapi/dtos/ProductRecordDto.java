7217f8a454c2ed3c2b4eb987e62996200c0dd3d4
Osvaldo Achar <osvaldofaachar@gmail.com>
2024-11-25 13:28:31 +0200

    Add files via upload

---
+++ src/main/java/com/osvaldo/productapi/dtos/ProductRecordDto.java
@@ -0,0 +1,10 @@
+package com.osvaldo.productapi.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
+
+}
